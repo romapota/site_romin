e2d4c54c4b732925fa70cb6a130ddf9395c1e5d5
romapota <[email>
2024-06-25 17:10:47 +0300

    First version hasn't adptive

--- romin/romin/settings.py
+++ romin/romin/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8)6c4d^n(9#@bf-ijr=h55k%3y-$6dz%=boa2_^q(8%q+ky@bq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['razrab2024.pythonanywhere.com']
+ALLOWED_HOSTS = ['razrab2024.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
